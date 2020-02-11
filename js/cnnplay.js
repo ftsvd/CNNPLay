30cde684d61d757d975df80e56dc9ec02923bc3c
Kim-Ann <52043226+ftsvd@users.noreply.github.com>
2020-02-11 20:53:08 +0800

    Fixed Training Issue

--- js/cnnplay.js
+++ js/cnnplay.js
@@ -12,6 +12,9 @@ var testSet = {};
 testSet.image = [];
 testSet.label = [];
 
+// maps labels(0,1,2,3,...) to labelText(CXR,AXR,...)
+var labels = [];
+
 var dict = {};
 dict['input'] = 'Input';
 dict['softmax'] = 'Softmax';
@@ -38,29 +41,35 @@ var dataFolder = 'classCXRAXR';
 // START
 function start() {
 
+	// Load labels
+	var file = 'labels.csv';
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			// Load csv into label and labelText arrays
+			var csvArray = this.responseText.split('\n');
+			
+			// Filter empty elements
+			// https://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript
+			csvArray = csvArray.filter(function (el) {
+				return el != "";
+			});	
+			
+			for (var i = 0; i < csvArray.length; i++) {
+				var csvLine = csvArray[i].split(',');
+				labels[csvLine[0]] = csvLine[1];
+			}
+			
+			// Begin loading image data
+			startLoadData();
+		}
+	};
+	xhttp.open('GET', dataFolder + '/' + file, true);
+	xhttp.send();
+
 	// Current working image & canvas
 	var tempCanvas = document.getElementById('tempCanvas');
 	tempCTX = tempCanvas.getContext('2d');
-
-	var set = ['train', 'val', 'test'];
-
-	// Retrieve and load training data	
-	var xhttp = [];
-	for (var i = 0; i < set.length; i++) {
-
-		var file = set[i] + '.csv';
-		xhttp[i] = new XMLHttpRequest();
-		xhttp[i].set = set[i];
-		xhttp[i].onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				document.getElementById('span' + this.set + 'Status').innerHTML = 'Ready!';
-				loadData(this.responseText, this.set);
-			}
-		};
-		xhttp[i].open('GET', dataFolder + '/' + file, true);
-		document.getElementById('span' + set[i] + 'Status').innerHTML = 'Loading...';
-		xhttp[i].send();
-	}
 	
 	// Adjust elements
 	// adjust tabs
@@ -79,6 +88,29 @@ function start() {
 	drawNeuralNet();	
 }
 
+// Called after labels are loaded
+function startLoadData() {
+	var set = ['train', 'val', 'test'];
+
+	// Retrieve and load training data	
+	var xhttp = [];
+	for (var i = 0; i < set.length; i++) {
+
+		var file = set[i] + '.csv';
+		xhttp[i] = new XMLHttpRequest();
+		xhttp[i].set = set[i];
+		xhttp[i].onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById('span' + this.set + 'Status').innerHTML = 'Ready!';
+				loadData(this.responseText, this.set);
+			}
+		};
+		xhttp[i].open('GET', dataFolder + '/' + file, true);
+		document.getElementById('span' + set[i] + 'Status').innerHTML = 'Loading...';
+		xhttp[i].send();
+	}	
+}
+
 // Load data from CSV to Image and Labels
 function loadData(csv, whichSet) {
 	
@@ -88,12 +120,11 @@ function loadData(csv, whichSet) {
 	
 	// Load csv into image and label arrays
 	var csvArray = csv.split('\n');
-//	console.log(csvArray);
 	
 	// Filter empty elements
 	// https://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript
 	csvArray = csvArray.filter(function (el) {
-	  return el != "";
+		return el != "";
 	});	
 
 	// Create Image objects and fill label array
@@ -101,11 +132,12 @@ function loadData(csv, whichSet) {
 		var csvLine = csvArray[i].split(',');
 		image[i] = new Image();
 		image[i].src = dataFolder + '/' + csvLine[0];
+		// Set style for display purposes
 		image[i].style.width = '100px';
 		image[i].style.height = '100px';
-		label[i] = csvLine[1];
+		label[i] = parseInt(csvLine[1]);
 		document.getElementById('span' + whichSet).appendChild(image[i]);
-		document.getElementById('span' + whichSet).innerHTML += '<br>' + label[i] + '<hr>';
+		document.getElementById('span' + whichSet).innerHTML += '<br>' + labels[label[i]] + '<hr>';
 	}
 	
 	// Assign Images and Labels to Sets
@@ -371,6 +403,17 @@ function loadPreConfigNet() {
 			'pool,2,2',
 			'fc,10,sigmoid',
 			'softmax,2' ];	
+
+	preConfigNet['USANOTAI'] =
+		[ 	'input,' + imageSize + ',' + imageSize + ',1',
+			'conv,16,5,1,2,relu',
+			'pool,2,2',
+			'conv,20,5,1,2,relu',
+			'pool,2,2',
+			'conv,20,5,1,2,relu',
+			'pool,2,2',
+			'softmax,2' ];	
+
 	
 	var whichNet = document.getElementById('selectPreConfigNet').value;
 	layers = preConfigNet[whichNet];
@@ -380,35 +423,57 @@ function loadPreConfigNet() {
 // Create Neural Network
 var net = new convnetjs.Net();
 function generateNetwork() {
+	
+	// Draw neural network summary
+	var spanNetSummaryHTML = '';
+	
 	// Create Layers
 	var layer_defs = [];
 	
 	for (var i = 0; i < layers.length; i++) {
 		var layerCSV = layers[i].split(',');
+
+		// Parse numbers into integers
+		for (var l = 0; l < layerCSV.length; l++) {
+			if (!isNaN(parseInt(layerCSV[l]))) {
+				layerCSV[l] = parseInt(layerCSV[l]);
+			}
+		}
 		
+		spanNetSummaryHTML += '<font size=+1><b>' + (i+1) + ': ' + dict[layerCSV[0]] + '</b></font><br>';
 		if (layerCSV[0] == 'input') {
 			layer_defs.push({type:'input', out_sx:layerCSV[1], out_sy:layerCSV[2], out_depth:layerCSV[3]});
+			spanNetSummaryHTML += layerCSV[1] + 'x' + layerCSV[2] + 'x' + layerCSV[3];
 		}
 
 		if (layerCSV[0] == 'fc') {
 			layer_defs.push({type:'fc', num_neurons:layerCSV[1], activation:layerCSV[2]})
+			spanNetSummaryHTML += layerCSV[1] + '| Act: ' + layerCSV[2];
 		}
 
 		if (layerCSV[0] == 'conv') {
 			layer_defs.push({type:'conv', sx:layerCSV[2], filters:layerCSV[1], stride:layerCSV[3], pad:layerCSV[4], activation:layerCSV[5]});
+			spanNetSummaryHTML += layerCSV[2] + 'x' + layerCSV[2] + '(' + layerCSV[1] + ')';
+			spanNetSummaryHTML += '<br>Stride: ' + layerCSV[3];
+			spanNetSummaryHTML += ' | Padding: ' + layerCSV[4];
+			spanNetSummaryHTML += ' | Act: ' + layerCSV[5];
 		}
 
 		if (layerCSV[0] == 'pool') {
 			layer_defs.push({type:'pool', sx:layerCSV[1], stride:layerCSV[2]})
+			spanNetSummaryHTML += layerCSV[1] + 'x' + layerCSV[1];
+			spanNetSummaryHTML += ' | Stride: ' + layerCSV[2];
 		}
 
 		if (layerCSV[0] == 'softmax') {
 			layer_defs.push({type:'softmax', num_classes:layerCSV[1]});
+			spanNetSummaryHTML += 'Classes: ' + layerCSV[1];
 		}
+		spanNetSummaryHTML += '<hr size=1>';
 		
 	}
-	net.makeLayers(layer_defs);
-	startTraining();
+	net.makeLayers(layer_defs);	
+	document.getElementById('spanNetSummary').innerHTML = spanNetSummaryHTML;
 }
 
 function startTraining() {
@@ -416,16 +481,26 @@ function startTraining() {
 	var then = new Date();
 
 	var epochs = document.getElementById('inputEpochs').value;
+
 	// Iterate through epochs
-	for (var e = 0; e < epochs; e++) {
+	// https://stackoverflow.com/questions/30987218/update-progressbar-in-each-loop/31654481
+	// for (var e = 0; e < epochs; e++) {
+	var e = 0;
+	(function iterateEpochs() {
+		
 		// Iterate through images in training set
-		for (var i = 0; i < trainSet.label.length; i++) {
-			
+		// https://stackoverflow.com/questions/30987218/update-progressbar-in-each-loop/31654481
+		// for (var i = 0; i < trainSet.label.length; i++) {
+		var i = 0;
+		(function iterateImages() {
+
 			//Create new training volume at imageSizeximageSizex1, pre-filled with 0.0
 			var trainVol = new convnetjs.Vol(imageSize, imageSize, 1, 0.0);
 			
 			//Get dimensionless imagedata
 			var trainVolData = loadImageIntoVolume(trainSet.image[i]);
+
+			//Force repaint
 
 			//Fill training volume with dimensionless imagedata
 			var p = 0;
@@ -438,16 +513,141 @@ function startTraining() {
 
 			//Initiate training
 			var trainer = new convnetjs.SGDTrainer(net, {method:'adadelta', batch_size:1, l2_decay:0.001});
+			// var trainer = new convnetjs.SGDTrainer(net, {learning_rate:0.2, momentum:0.1, batch_size:1, l2_decay:0.001});
+			
 			trainer.train(trainVol, trainSet.label[i]); //train using matched trainVol and classifier
 
-		}
+
+			i++;
+			if (i < trainSet.label.length) {
+				setTimeout(iterateImages, 0);
+			}
+		})();
+		// }
 		// Do validation every Epoch
+
+		// Blank the result span
+		document.getElementById('spanValResults').innerHTML = '';
 		
-	}
+		// Iterate through images in validation set
+		for (var i = 0; i < valSet.label.length; i++) {
+		
+			//PREDICT
+			//initiate testing volume at imageSizevximageSizex1 prefilled with 0.0
+			var valVol = new convnetjs.Vol(imageSize, imageSize, 1, 0.0);
+
+			//Get dimensionless imagedata
+			var valVolData = loadImageIntoVolume(valSet.image[i]);
+
+			//fill testing volume with dimensionless imagedata of test volumes
+			var p = 0;
+			for (var yc = 0; yc < imageSize; yc++) {
+				for (var xc = 0; xc < imageSize; xc++) {
+					valVol.set(xc, yc, 0, valVolData[p]);
+					p++;
+				}
+			}
+
+			var prediction = net.forward(valVol);
+			
+			//pick the label with the highest score
+			var highestWeight = 0;
+			var highestWeightedLabel = null;
+			for (var o = 0; o < labels.length; o++) {
+				if (prediction.w[o] > highestWeight) {
+					highestWeight = prediction.w[o];
+					highestWeightedLabel = o;
+				}
+			}
+			
+			// Output the predictions
+			document.getElementById('spanValResults').appendChild(valSet.image[i]);
+			
+			// Color the output whether match or not
+			if (highestWeightedLabel == valSet.label[i]) {
+				var predictionColor = '#0000ff';
+			} else {
+				var predictionColor = '#ff0000';
+			}
+			var spanValResultsHTML = '<br>';
+			spanValResultsHTML += 'Ground Truth: ' + labels[valSet.label[i]] + '<br>';
+			spanValResultsHTML += 'Prediction: <font color="' + predictionColor + '">' + labels[highestWeightedLabel] + '(' + prediction.w[highestWeightedLabel].toFixed(3) + ')</font>';
+			spanValResultsHTML += '<hr>';
+			document.getElementById('spanValResults').innerHTML += spanValResultsHTML;
+		}
+
+
+		e++;
+		if (e < epochs) {
+			setTimeout(iterateEpochs, 0);
+		}
+	})();
+	// }
 	
 	//TIMER END:
 	var now = new Date();
 	console.log('Train time taken: ' + (now - then) + 'ms');
+}
+
+function doTest() {
+	// Blank the result span
+	document.getElementById('spanTestResults').innerHTML = '';
+	
+	// Iterate through images in testing set
+	// https://stackoverflow.com/questions/30987218/update-progressbar-in-each-loop/31654481
+	// for (var i = 0; i < testSet.label.length; i++) {
+	var i = 0;
+	
+	(function iterateImages() {
+	
+		//PREDICT
+		//initiate testing volume at imageSizevximageSizex1 prefilled with 0.0
+		var testVol = new convnetjs.Vol(imageSize, imageSize, 1, 0.0);
+
+		//Get dimensionless imagedata
+		var testVolData = loadImageIntoVolume(testSet.image[i]);
+
+		//fill testing volume with dimensionless imagedata of test volumes
+		var p = 0;
+		for (var yc = 0; yc < imageSize; yc++) {
+			for (var xc = 0; xc < imageSize; xc++) {
+				testVol.set(xc, yc, 0, testVolData[p]);
+				p++;
+			}
+		}
+
+		var prediction = net.forward(testVol);
+		
+		//pick the label with the highest score
+		var highestWeight = 0;
+		var highestWeightedLabel = null;
+		for (var o = 0; o < labels.length; o++) {
+			if (prediction.w[o] > highestWeight) {
+				highestWeight = prediction.w[o];
+				highestWeightedLabel = o;
+			}
+		}
+		
+		// Output the predictions
+		document.getElementById('spanTestResults').appendChild(testSet.image[i]);
+		
+		// Color the output whether match or not
+		if (highestWeightedLabel == testSet.label[i]) {
+			var predictionColor = '#0000ff';
+		} else {
+			var predictionColor = '#ff0000';
+		}
+		var spanTestResultsHTML = '<br>';
+		spanTestResultsHTML += 'Ground Truth: ' + labels[testSet.label[i]] + '<br>';
+		spanTestResultsHTML += 'Prediction: <font color="' + predictionColor + '">' + labels[highestWeightedLabel] + '(' + prediction.w[highestWeightedLabel].toFixed(3) + ')</font>';
+		spanTestResultsHTML += '<hr>';
+		document.getElementById('spanTestResults').innerHTML += spanTestResultsHTML;
+
+		i++;
+		if (i < testSet.label.length) {
+			setTimeout(iterateImages, 0);
+		}
+	})();
 }
 
 // Return dimensionless imagedata from trainingImage
@@ -462,7 +662,8 @@ function loadImageIntoVolume(trainingImage) {
 	}
 	// Following console.log to ensure that tempImage.src is loaded and visually different from previous loaded image
 	// FUTURE RECOMMENDATION: need to fix with onLoad event
-	console.log(progressCounter + ":" + tempImageDataGray[(imageSize * imageSize / 2) - (imageSize / 2)], tempImageDataGray.length); //center pixel
+	console.log(trainingImage.src);
+	//console.log(progressCounter + ":" + tempImageDataGray[(imageSize * imageSize / 2) - (imageSize / 2)], tempImageDataGray.length); //center pixel
 	progressCounter++;
 	return tempImageDataGray;
 }
@@ -481,6 +682,15 @@ function showTab(whichTab) {
 }
 
 
+
+// Debug Parts
+
+// console.logs the model as a string, either to be copy-pasted or saved
+function doNet() {
+	var json = net.toJSON();
+	var str = JSON.stringify(json);
+	console.log(str);
+}
 
 
 
